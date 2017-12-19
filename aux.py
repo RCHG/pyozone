e941101c44acbe1944a8cc89e62d7ffd67f430f5
RCHG <RCHG@users.noreply.github.com>
2017-12-19 14:18:27 +0000

    Update aux.py

--- aux.py
+++ aux.py
@@ -29,6 +29,9 @@ __project__      = "SMURPHS and CMIP6 ozone database"
 __email__        = "r.checa-garcia@reading.ac.uk"
 __status__       = "Consolidating"
 ------------------------------------------------------------------------------
+History:
+    - Small refactory of the code in Nov-2017
+
 """
 
 import numpy as np
